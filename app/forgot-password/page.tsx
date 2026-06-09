5c3724dce892d74b447ca36f144bd1fe5683321e
tlewis <tabslewis@me.com>
2026-06-09 13:19:39 -0700

    Update page.tsx

--- app/forgot-password/page.tsx
+++ app/forgot-password/page.tsx
@@ -32,7 +32,7 @@ export default function ForgotPasswordPage() {
     
     const supabase = createClient()
     const { error } = await supabase.auth.resetPasswordForEmail(email, {
-      redirectTo: `${window.location.origin}/reset-password`,
+     redirectTo: `https://homesuitemanager.com/auth/callback?type=recovery&next=/reset-password`,
     })
     
     if (error) {
