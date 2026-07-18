30d3e7b57acfdaf094c47868db02367ae40e17a2
tlewis <tabslewis@me.com>
2026-07-17 23:52:15 -0700

    Update server.ts

--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -1,4 +1,4 @@
-import { createClient } from "@/lib/supabase/server"
+import { createServerClient } from '@supabase/ssr'
 import { NextResponse, type NextRequest } from "next/server"
 
 export async function createClient() {
